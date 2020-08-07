3f437307e2ddeb287b4201a857efd65a2f9517b5
Douxie14 <elodie.reze@gmail.com>
2020-08-07 15:11:45 +0200

    fetchOk

---
+++ Frontend/index.js
@@ -0,0 +1,18 @@
+const teddiesList = document.getElementById("bloc_image");
+
+async function teddiesInformation (url) {
+    let result = await fetch(url)
+    return result.json()
+}
+
+teddiesInformation ('http://localhost:3000/api/teddies') 
+.then (teddies => {
+    teddies.forEach(teddy => {
+        console.log(teddy)  
+        teddiesList.innerHTML += `<div><img
+         src="${teddy.imageUrl}"></div>`
+    })         
+})
+.catch (error => {
+    console.log("nous avons trouvé " + error)
+});
